322fcbe2640fe1eaa1ea75febb94111f1a14feb4
Admin <prasad.parshram123@gmail.com>
2022-05-11 16:04:39 +0530

    design bacon details screen

--- lib/screens/beacon_detail_screen.dart
+++ lib/screens/beacon_detail_screen.dart
@@ -5,6 +5,46 @@ class BeaconDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Beacon details"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              headingWidget("UUID"),
+              Text("sdfdsfdsfsdfsdfsdfdsfsfs"),
+              spacingWidget(),
+              headingWidget("BT Address"),
+              Text("sdfdsfdsfsdfsdfsdfdsfsfs"),
+              spacingWidget(),
+              headingWidget("Major Minor"),
+              Text("sdfdsfdsfsdfsdfsdfdsfsfs"),
+              spacingWidget(),
+              headingWidget("Distance"),
+              Text("1.2 m"),
+              spacingWidget(),
+              headingWidget("Tx Power"),
+              Text("-54 m "),
+              spacingWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+Widget spacingWidget() => const SizedBox(
+      height: 15.0,
+    );
+
+Widget headingWidget(String heading) => Text(
+      heading,
+      textScaleFactor: 1.1,
+    );
