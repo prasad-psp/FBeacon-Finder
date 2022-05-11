fadcf447804f48243d58c3b33fec171560320b14
Admin <prasad.parshram123@gmail.com>
2022-05-11 15:26:45 +0530

    design home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Beacon Finder"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            LinearProgressIndicator(),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: 5,
+                itemBuilder: ((context, index) => BeaconListItem()),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.search),
+      ),
+    );
+  }
+}
+
+class BeaconListItem extends StatelessWidget {
+  const BeaconListItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.bubble_chart_rounded),
+      title: Text("dskjfhsjkdhfsdhfdshjkf"),
+      subtitle: Text("major minor"),
+      trailing: Text("-dsfk dbm"),
+      onTap: () {},
+    );
   }
 }
