eb1740db5bdfe53ea0da574cb9e3c5a17735b9f9
Admin <prasad.parshram123@gmail.com>
2022-05-11 11:27:31 +0530

    added app routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:fbeacon_finder/route/app_routes.dart';
+import 'package:fbeacon_finder/screens/beacon_detail_screen.dart';
+import 'package:fbeacon_finder/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -9,6 +12,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "FBeacon Finder",
       debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.initialRoute,
+      onGenerateRoute: (setting) {
+        switch (setting.name) {
+          case AppRoutes.initialRoute:
+            {
+              return MaterialPageRoute(
+                  builder: (context) => const HomeScreen());
+            }
+          case AppRoutes.beaconDetail:
+            {
+              return MaterialPageRoute(
+                  builder: (context) => const BeaconDetailScreen());
+            }
+        }
+      },
     );
   }
 }
---
+++ lib/route/app_routes.dart
@@ -0,0 +1,4 @@
+class AppRoutes {
+  static const initialRoute = "/home";
+  static const beaconDetail = "/beacon_detail";
+}
